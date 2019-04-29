4792d9cbe2d80dbe67371b120ef2b6d1878bec95
我不吃植物 <2756145382@qq.com>
2019-04-29 13:54:19 +0800

    完善卡券同步接口

--- src/Rpc/WechatCard.php
+++ src/Rpc/WechatCard.php
@@ -4,15 +4,32 @@ namespace Ineplant\Rpc;
 
 class WechatCard extends WechatBasic {
 
-    public static function create($appId, $data) {
+
+    /**
+     * @param $appId
+     * @param $data
+     * @param string $type
+     * @return mixed|\Psr\Http\Message\ResponseInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function create($appId, $data, $type = 'member_card') {
         return self::getClient()->request('POST', "/api/card/create", [
             'json' => [
                 'appId' => $appId,
                 'data' => $data,
+                'type' => $type,
             ]
         ]);
     }
 
+
+    /**
+     * @param $appId
+     * @param $cardId
+     * @param $code
+     * @return mixed|\Psr\Http\Message\ResponseInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
     public static function getUser($appId, $cardId, $code) {
         return self::getClient()->request('GET', "/api/card/user/get", [
             'json' => [
@@ -23,6 +40,13 @@ class WechatCard extends WechatBasic {
         ]);
     }
 
+
+    /**
+     * @param $appId
+     * @param $data
+     * @return mixed|\Psr\Http\Message\ResponseInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
     public static function updateUser($appId, $data) {
         return self::getClient()->request('POST', "/api/card/user/update/$appId", [
             'json' => [
@@ -32,6 +56,14 @@ class WechatCard extends WechatBasic {
         ]);
     }
 
+
+    /**
+     * @param $appId
+     * @param $cardId
+     * @param $settings
+     * @return mixed|\Psr\Http\Message\ResponseInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
     public static function setActivationForm($appId, $cardId, $settings) {
         return self::getClient()->request('POST', "/api/card/user/update/$cardId", [
             'json' => [
@@ -41,9 +73,35 @@ class WechatCard extends WechatBasic {
         ]);
     }
 
+
+    /**
+     * @param $appId
+     * @param $info
+     * @return mixed|\Psr\Http\Message\ResponseInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
     public static function activate($appId, $info) {
         return self::getClient()->request('POST', "/api/card/activity/$appId", [
             'json' => $info
+        ]);
+    }
+
+
+    /**
+     * @param $appId
+     * @param $cardId
+     * @param $attributes
+     * @param string $type
+     * @return mixed|\Psr\Http\Message\ResponseInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function update($appId, $cardId, $attributes, $type = 'member_card') {
+        return self::getClient()->request('POST', "/api/card/update/$cardId", [
+            'json' => [
+                'appId' => $appId,
+                'type' => $type,
+                'attributes' => $attributes,
+            ]
         ]);
     }
 
