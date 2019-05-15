951ee9183417fc8c4d778f74a47ed4a2b8ca1c93
我不吃植物 <2756145382@qq.com>
2019-05-15 20:21:50 +0800

    修复bug

--- src/Rpc/BaseRpc.php
+++ src/Rpc/BaseRpc.php
@@ -15,8 +15,23 @@ trait BaseRpc {
      * @return Client
      */
     public static function getClient() {
+
+        $env = getenv("SERVICE_ENV");
+
+        $domain = self::$domain;
+
+        if (!empty($env)) {
+            $protocol = 'https://';
+
+            if (false == strpos($domain, 'https://')) {
+                $protocol = 'http://';
+            }
+
+            str_replace($protocol,"$protocol://$env-", $domain);
+        }
+
         empty(self::$client) && self::$client = new Client([
-            'base_uri' => self::$domain
+            'base_uri' => $domain
         ]);
 
         return self::$client;
