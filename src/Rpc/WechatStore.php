2351b1182edb35559eb4d10ab342b50113da6e29
我不吃植物 <2756145382@qq.com>
2019-05-27 19:23:12 +0800

    完善基本功能

--- src/Rpc/WechatStore.php
+++ src/Rpc/WechatStore.php
@@ -63,5 +63,75 @@ class WechatStore extends WechatBasic {
     }
 
 
+    /**
+     * @param $appId
+     * @param $baseInfo
+     * @return mixed|\Psr\Http\Message\ResponseInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function createFromMap($appId, $baseInfo) {
+        return self::getClient()->request('POST', "/api/store/create_from_map", [
+            'json' => [
+                'appId'  => $appId,
+                'baseInfo' => $baseInfo
+            ]
+        ]);
+    }
+
+    /**
+     * @param $appId
+     * @param $baseInfo
+     * @return mixed|\Psr\Http\Message\ResponseInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function create($appId, $baseInfo) {
+        return self::getClient()->request('POST', "/api/store/create", [
+            'json' => [
+                'appId'  => $appId,
+                'baseInfo' => $baseInfo
+            ]
+        ]);
+    }
+
+
+    /**
+     * @param $appId
+     * @param $baseInfo
+     * @return mixed|\Psr\Http\Message\ResponseInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function update($appId, $poiId, $baseInfo) {
+        return self::getClient()->request('POST', "/api/store/update/{$poiId}", [
+            'json' => [
+                'appId'  => $appId,
+                'baseInfo' => $baseInfo
+            ]
+        ]);
+    }
+
+
+    /**
+     * @param $appId
+     * @param $poiId
+     * @return \GuzzleHttp\Promise\PromiseInterface
+     */
+    public static function getDeleteRequest($appId, $poiId) {
+        return self::getClient()->requestAsync('POST', "/api/store/delete/{$poiId}", [
+            'json' => [
+                'appId'  => $appId
+            ]
+        ]);
+    }
+
+    /**
+     * @param $appId
+     * @param $baseInfo
+     * @return mixed|\Psr\Http\Message\ResponseInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function delete($appId, $poiId) {
+        return self::getDeleteRequest($appId, $poiId)->wait();
+    }
+
 
 }
