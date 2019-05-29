6f8d6832c65bbe2b107c8fa492591e2c7e87d78f
我不吃植物 <2756145382@qq.com>
2019-05-29 10:24:04 +0800

    优化业务

--- src/Rpc/WechatMedia.php
+++ src/Rpc/WechatMedia.php
@@ -11,7 +11,21 @@ class WechatMedia extends WechatBasic {
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public static function uploadImage($appId, $path) {
-        return self::getClient()->request('POST', "/api/media/up_image", [
+        return self::getMediaRequest()->wait();
+    }
+
+
+    /**
+     * @param $appId
+     * @param $path
+     * @param null $client
+     * @return \GuzzleHttp\Promise\PromiseInterface
+     */
+    public static function getMediaRequest($appId, $path, $client = null) {
+
+        $client = $client ?? self::getClient();
+
+        return $client->requestAsync('POST', "/api/media/up_image", [
             'json' => [
                 'path' => $path,
                 'appId' => $appId
