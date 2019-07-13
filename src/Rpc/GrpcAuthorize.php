77a83e299944cd19ec73b6fe1944c60ad916074f
我不吃植物 <2756145382@qq.com>
2019-07-13 18:23:31 +0800

    优化业务

---
+++ src/Rpc/GrpcAuthorize.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Ineplant\Rpc;
+
+use Authorize\AuthInfo;
+use Authorize\AuthorizeClient;
+
+class GrpcAuthorize extends GrpcClient {
+
+    //1是普通用户，2是商家用户，3是大后台用户
+    const User = 1;
+    const Company = 2;
+    const System = 3;
+
+
+    protected static function getClientName() {
+        return AuthorizeClient::class;
+    }
+
+    protected static function getServAddName(): string {
+        return "authorize-service:8080";
+    }
+
+
+    /**
+     * @param $companyId
+     * @param $userId
+     * @param int $type
+     * @return mixed
+     */
+    public static function auth($companyId, $userId, $type = self::Company) {
+        $request = new AuthInfo();
+
+        $request->setCompanyId($companyId);
+        $request->setType($type);
+        $request->setUserId($userId);
+
+        return self::getClient()->auth($request)->wait();
+    }
+
+
+}
