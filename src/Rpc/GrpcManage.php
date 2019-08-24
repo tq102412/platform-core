85f7443a6d3ef6bb37b4b6670af3adc17b438628
thxcud <thxcud@outlook.com>
2019-08-24 17:22:40 +0800

    manage rpc

---
+++ src/Rpc/GrpcManage.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace Ineplant\Rpc;
+
+
+use Manage\HiUser;
+use Manage\ManageClient;
+
+class GrpcManage extends GrpcClient {
+    /**
+     * @var 客户端实例
+     */
+    protected static $client;
+
+    protected static function getClientName() {
+        return ManageClient::class;
+    }
+
+    protected static function getServAddName(): string {
+        return "localhost:8080";
+    }
+
+    public static function hi($name, $sex) {
+        $client = self::getClient();
+
+        $request = new HiUser();
+        $request->setName($name);
+        $request->setSex($sex);
+
+        list($reply, $status) = $client->sayHello($request);
+        $message = $reply->getMessage();
+//        $user    = $reply->getUser();
+
+        $client->close();
+        return $message;
+    }
+}
