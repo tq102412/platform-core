e7fadae8420312340bc20516a5bd8656992b745e
我不吃植物 <2756145382@qq.com>
2019-09-25 09:41:41 +0800

    增加错误码

--- src/Enum/ErrorCode.php
+++ src/Enum/ErrorCode.php
@@ -34,6 +34,7 @@ class ErrorCode {
     const DATA_STATUS = 40003;
 
     const SQL = 50001; //sql执行错误
+    const COMPANY_NOT_FOUND = 50002; //店铺不存在
 
     const CODE = 99999; //代码错误
 
@@ -56,6 +57,7 @@ class ErrorCode {
         self::UNAUTHORIZED         => '没有权限',
         self::SYSTEM_FAIL          => '系统错误',
         self::UNION_INFO_NOT_FOUND => '没有找到关联信息',
+        self::COMPANY_NOT_FOUND    => '店铺不存在',
     ];
 
 
