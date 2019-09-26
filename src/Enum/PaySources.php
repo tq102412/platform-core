852431dd11a7415ae916dd199da6cb8bd62fd6c6
thxcud <thxcud@outlook.com>
2019-09-26 18:32:35 +0800

    回调方法 枚举

---
+++ src/Enum/PaySources.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Ineplant\Enum;
+
+
+class PaySources extends SplEnum {
+    //微信 充值
+    const RECHARGE  = 1;
+    //微信 消费
+    const CONSUME = 2;
+    //微信 应用订单
+    const APPLICATION_ORDER = 3;
+}
