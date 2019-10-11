694c8bc29ed19f9a2026d446efb6f77639759930
thxcud <thxcud@outlook.com>
2019-10-11 17:30:12 +0800

    swoole AuthClient

---
+++ src/Rpc/SwooleGrpcAuth.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Ineplant\Rpc;
+
+use Authorize\AuthBig;
+use Authorize\AuthFollow;
+use Authorize\AuthInfo;
+use Authorize\SwooleAuthClient;
+
+class SwooleGrpcAuth extends GrpcClient {
+
+    /**
+     * @var 客户端实例
+     */
+    protected static $client;
+
+    //1是普通用户，2是商家用户，3是大后台用户
+    const User = 1;
+    const Company = 2;
+    const System = 3;
+
+
+    protected static function getClientName() {
+        return SwooleAuthClient::class;
+    }
+
+    protected static function getServAddName(): string {
+        return "authorize:8080";
+    }
+
+    /**
+     * @param $companyId
+     * @param $userId
+     * @param $followId
+     * @return mixed
+     */
+    public static function auth($companyId, $userId, $followId) {
+        $request = new AuthInfo();
+
+        $request->setCompanyId($companyId);
+        $request->setUserId($userId);
+        $request->setFollowId($followId);
+
+        return self::getClient()->auth($request)->wait();
+    }
+
+    /**
+     * @param $followId
+     * @return mixed
+     */
+    public static function authFollow($followId) {
+        $request = new AuthFollow();
+
+        $request->setFollowId($followId);
+
+        return self::getClient()->authFollow($request)->wait();
+    }
+
+
+    /**
+     * @param int $uid
+     * @return mixed
+     */
+    public static function authBig(int $uid) {
+        $request = new AuthBig();
+
+        $request->setUid($uid);
+
+        return self::getClient()->authBig($request)->wait();
+    }
+
+
+}
