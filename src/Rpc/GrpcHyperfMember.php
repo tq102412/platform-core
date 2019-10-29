3bed0b4c4922b9bfe4373d5564d71aa215c79acb
thxcud <thxcud@outlook.com>
2019-10-29 18:36:49 +0800

    grpc Client

--- src/Rpc/GrpcHyperfMember.php
+++ src/Rpc/GrpcHyperfMember.php
@@ -3,14 +3,32 @@
 
 namespace Ineplant\Rpc;
 
+use Member\GetByMemberUnionIds;
 use Member\MembersHyprfClient;
 
-class GrpcHyperfMember extends GrpcMember {
+class GrpcHyperfMember {
 
     /**
-     * @return string
+     * @return MembersHyprfClient|mixed
      */
-    protected static function getClientName() {
-        return MembersHyprfClient::class;
+    protected static function getClient() {
+        return new MembersHyprfClient('member-service:8080', [
+            'credentials' => null,
+        ]);
     }
+
+    /**
+     * 获取会员信息通过union_id
+     *
+     * @param array $unionIds
+     * @return mixed
+     */
+    public static function getListByUnionIds(array $unionIds) {
+        $request = new GetByMemberUnionIds();
+
+        $request->setUnionIds($unionIds);
+
+        return self::getClient()->GetListByUnionId($request);
+    }
+
 }
