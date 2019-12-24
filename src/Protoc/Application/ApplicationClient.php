884e79325588f4029bd2e0b42dfe499bc550d665
thxcud <thxcud@outlook.com>
2019-12-24 11:00:15 +0800

    试用套餐发放rpc

--- src/Protoc/Application/ApplicationClient.php
+++ src/Protoc/Application/ApplicationClient.php
@@ -7,6 +7,12 @@ namespace Application;
 use Hyperf\GrpcClient\BaseClient;
 
 class ApplicationClient extends BaseClient {
+    /**
+     * 应用订单回调
+     *
+     * @param Order $order
+     * @return array|\Google\Protobuf\Internal\Message[]|\Grpc\StringifyAble[]|\swoole_http2_response[]
+     */
     public function appOrderNotify(Order $order) {
         return $this->simpleRequest(
             '/application.application/appOrderNotify',
@@ -14,4 +20,19 @@ class ApplicationClient extends BaseClient {
             [Result::class, 'decode']
         );
     }
+
+    /**
+     * 自动发放使用套餐
+     *
+     * @param User $user
+     * @return array|\Google\Protobuf\Internal\Message[]|\Grpc\StringifyAble[]|\swoole_http2_response[]
+     */
+    public function trialSend(User $user) {
+        return $this->simpleRequest(
+            '/application.application/trialSend',
+            $user,
+            [Result::class, 'decode']
+        );
+    }
+
 }
