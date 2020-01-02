20abbc0fd9a88a46859f9d051715cf6354f5edd7
thxcud <thxcud@outlook.com>
2020-01-02 09:08:02 +0800

    codePayWithResult 方法

--- src/Rpc/WechatPayment.php
+++ src/Rpc/WechatPayment.php
@@ -2,6 +2,9 @@
 
 namespace Ineplant\Rpc;
 
+use Ineplant\Exceptions\ReturnException;
+use Ineplant\Helper;
+
 class WechatPayment extends WechatBasic {
 
 
@@ -23,25 +26,25 @@ class WechatPayment extends WechatBasic {
 
     /**
      * 支付码扣款并获取到支付结果
-     *
      * @param array $company {
      * @type string $company_id
      * @type string $name
      * }
      * @param string $authCode 付款码
      * @param string $totalFee 支付金额 (分)
-     * @return bool
-     * @return mixed|\Psr\Http\Message\ResponseInterface
+     * @return mixed transaction_id
+     * @throws ReturnException
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public static function codePayWithResult($company, $authCode, $totalFee) {
-        return self::getClient()->request('POST', "/api/wx/pay/code", [
+        $response = self::getClient()->request('POST', "/api/wx/pay/code", [
             'json' => [
                 'company'   => $company,
                 'auth_code' => $authCode,
                 'total_fee' => $totalFee,
             ]
         ]);
+        return Helper::HttpRpcResult($response);
     }
 
     /**
