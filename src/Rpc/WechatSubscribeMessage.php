ab64ae87353cb21b26d03af4198e2cc919713607
thxcud <thxcud@outlook.com>
2020-01-16 16:16:26 +0800

    群发小程序模板消息

--- src/Rpc/WechatSubscribeMessage.php
+++ src/Rpc/WechatSubscribeMessage.php
@@ -41,4 +41,35 @@ class WechatSubscribeMessage extends WechatBasic {
         return self::send($appId, $json);
     }
 
+    /**
+     * @param $appId
+     * @param $templateId
+     * @param $openIds
+     * @param $data
+     * @return mixed|\Psr\Http\Message\ResponseInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function sendWinningResultByOpenIds($appId, $templateId, $openIds, $data) {
+        $json = [
+            'appid'    => $appId,
+            'open_ids' => $openIds,
+            'info'     => [
+                'template_id' => $templateId,
+                'page'        => 'pages/home/index/index',
+                'data'        => [
+                    'thing1' => $data[0], // 开奖结果
+                    'thing2' => $data[1], // 活动名称
+                    'thing3' => $data[2], // 商家名称
+                    'date4'  => $data[3], // 开奖时间
+                    'thing5' => $data[4], // 备注
+                ],
+            ]
+
+        ];
+
+        return self::getClient()->request('POST', "/api/subscribe/message/batch/send", [
+            'json' => $json,
+        ]);
+    }
+
 }
