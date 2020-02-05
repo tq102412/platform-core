2f8ecf1f3f2aba807123393d2d89924992763926
thxcud <444382247@qq.com>
2020-02-05 13:56:15 +0800

    添加支付类型

--- src/Enum/PaySources.php
+++ src/Enum/PaySources.php
@@ -6,9 +6,11 @@ namespace Ineplant\Enum;
 
 class PaySources extends SplEnum {
     //微信 充值
-    const RECHARGE  = 1;
+    const RECHARGE = 1;
     //微信 消费
     const CONSUME = 2;
     //微信 应用订单
     const APPLICATION_ORDER = 3;
+    //发布(红包类)活动订单
+    const PUBLISH_ACTIVITY = 4;
 }
