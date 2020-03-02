f554a4a680c93af12482e6265089214e00fd5f0a
我不吃植物 <2756145382@qq.com>
2020-03-02 16:14:58 +0800

    增加错误码

--- src/Enum/ErrorCode.php
+++ src/Enum/ErrorCode.php
@@ -19,8 +19,8 @@ class ErrorCode {
 
     const SECRET = 10002; //密码错误
 
-    const POWER              = 20000; //一般权限不足
-    const PEER_POWER         = 20001; //同级权限不匹配,例如可以为别人助力无法为自己助力
+    const POWER = 20000; //一般权限不足
+    const PEER_POWER = 20001; //同级权限不匹配,例如可以为别人助力无法为自己助力
     const APP_PURCHASE_POWER = 21001; //应用购买失效
 
     const API = 30000; //API调用错误
@@ -29,22 +29,25 @@ class ErrorCode {
     const REDIRECT = 302; //需要重定向
 
     //数据 数量/时间/状态 有误
-    const DATA                 = 40000;
-    const DATA_NUM             = 40001;
-    const DATA_TIME            = 40002;
-    const DATA_STATUS          = 40003;
+    const DATA = 40000;
+    const DATA_NUM = 40001;
+    const DATA_TIME = 40002;
+    const DATA_STATUS = 40003;
     const UNION_INFO_NOT_FOUND = 40004;
-    const MODEL_NOT_FOUND      = 40005; //数据查询失败
-    const NEED_OPTIMIZATION    = 40006; //需要完善资料
-    const NOT_ALL_SUCCESSFUL   = 40007; //未全部成功
+    const MODEL_NOT_FOUND = 40005; //数据查询失败
+    const NEED_OPTIMIZATION = 40006; //需要完善资料
+    const NOT_ALL_SUCCESSFUL = 40007; //未全部成功
+    const UNBIND_WECHAT = 40008; //未绑定公众号
 
-    const CONFIG_ADMIN    = 40010;//管理员配置错误
+    const NOT_WECHAT_VERIFY_SERVICE = 40009; //不是微信认证服务号
+    const CONFIG_ADMIN = 40010;//管理员配置错误
     const CONFIG_BUSINESS = 40011;//B端用户配置错误
+    const NOT_WECHAT_VERIFY = 40012; // 公众号没有认证
 
     //模板消息的行业不符，但修改失败：本月已超出修改次数
     const TEMPLATE_INDUSTRY_ERROR = 43100;
 
-    const SQL               = 50001; //sql执行错误
+    const SQL = 50001; //sql执行错误
     const COMPANY_NOT_FOUND = 50002; //店铺不存在
 
     const TEXT_ANTI_SPAM = 61011;//文本反垃圾未通过
@@ -52,26 +55,29 @@ class ErrorCode {
     const CODE = 99999; //代码错误
 
     const ErrorMsg = [
-        self::PARAM                   => '参数错误',
-        self::ID                      => '参数错误',
-        self::SECRET                  => '参数错误',
-        self::POWER                   => '没有权限',
-        self::PEER_POWER              => '没有权限',
-        self::API                     => 'API调用错误',
-        self::REDIRECT                => '需要重定向',
-        self::DATA                    => '数据有误',
-        self::DATA_NUM                => '数据有误',
-        self::DATA_TIME               => '数据有误',
-        self::SQL                     => '未知错误',
-        self::CODE                    => '代码错误',
-        self::NOT_FOUND               => '资源不存在',
-        self::UNAUTHORIZED            => '没有权限',
-        self::SYSTEM_FAIL             => '系统错误',
-        self::UNION_INFO_NOT_FOUND    => '没有找到关联信息',
-        self::COMPANY_NOT_FOUND       => '店铺不存在',
-        self::TEXT_ANTI_SPAM          => '含有敏感内容',
-        self::MODEL_NOT_FOUND         => '未查询到相关数据',
-        self::TEMPLATE_INDUSTRY_ERROR => '模板消息的行业不符，但修改失败：本月已超出修改次数',
+        self::PARAM                     => '参数错误',
+        self::ID                        => '参数错误',
+        self::SECRET                    => '参数错误',
+        self::POWER                     => '没有权限',
+        self::PEER_POWER                => '没有权限',
+        self::API                       => 'API调用错误',
+        self::REDIRECT                  => '需要重定向',
+        self::DATA                      => '数据有误',
+        self::DATA_NUM                  => '数据有误',
+        self::DATA_TIME                 => '数据有误',
+        self::SQL                       => '未知错误',
+        self::CODE                      => '代码错误',
+        self::NOT_FOUND                 => '资源不存在',
+        self::UNAUTHORIZED              => '没有权限',
+        self::SYSTEM_FAIL               => '系统错误',
+        self::UNION_INFO_NOT_FOUND      => '没有找到关联信息',
+        self::COMPANY_NOT_FOUND         => '店铺不存在',
+        self::TEXT_ANTI_SPAM            => '含有敏感内容',
+        self::MODEL_NOT_FOUND           => '未查询到相关数据',
+        self::TEMPLATE_INDUSTRY_ERROR   => '模板消息的行业不符，但修改失败：本月已超出修改次数',
+        self::UNBIND_WECHAT             => '未绑定公众号',
+        self::NOT_WECHAT_VERIFY_SERVICE => '不是微信认证服务号',
+        self::NOT_WECHAT_VERIFY         => '公众号没有认证',
     ];
 
 
