ecadfc1c989216b311953aa85d1c1c984bfb8bb2
thxcud <thxcud@outlook.com>
2020-03-24 15:23:27 +0800

    更规范统一的订单状态

--- src/Enum/OrderStatus.php
+++ src/Enum/OrderStatus.php
@@ -8,11 +8,11 @@ class OrderStatus extends SplEnum {
     //未支付
     const UNPAID = 0;
 
-    //已支付
-    const PAID = 10;
+    //已支付/未生效
+    const PAID_NOT_EFFECTIVE = 5;
 
-    //待发货(已成团)
-    const WAIT_DELIVERY = 12;
+    //已支付/待发货
+    const PAID = 10;
 
     //退款成功
     const REFUNDED = 20;
