ac647c1bfbe6640960e589eb9a6a25406951dbc7
thxcud <thxcud@outlook.com>
2020-04-17 13:42:25 +0800

    添加红包下单的支付来源枚举

--- src/Enum/PaySources.php
+++ src/Enum/PaySources.php
@@ -9,8 +9,12 @@ class PaySources extends SplEnum {
     const RECHARGE = 1;
     //微信 消费
     const CONSUME = 2;
+
     //微信 应用订单,红包下单
     const APPLICATION_ORDER = 3;
+    //红包活动发布下单(回调到application中)
+    const PACKET_ACT_ORDER = 5;
+
     //微信 活动端订单
     const ACTIVITY_ORDER = 4;
 }
