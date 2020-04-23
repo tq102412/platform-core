04c6ad546b21eaf08c25a88bca65dc8907779ff6
thxcud <thxcud@outlook.com>
2020-04-23 09:38:27 +0800

    微信事件[发送带场景的活动]的key 定义

--- src/Enum/WxEventKeys.php
+++ src/Enum/WxEventKeys.php
@@ -8,4 +8,5 @@ class WxEventKeys {
     const SHARE_SUBSCRIBE = '_share__';
     const COMPANY_SHARE_SUBSCRIBE = '_companyshare__';
     const LOGIN = '_log__';
+    const ACTIVITY = '_activity__';
 }
