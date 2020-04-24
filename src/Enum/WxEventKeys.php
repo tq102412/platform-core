08ac2404f8a3dd80497390480650a5c824ff5f2a
thxcud <thxcud@outlook.com>
2020-04-24 16:49:49 +0800

    活动引导关注的图文信息生成

--- src/Enum/WxEventKeys.php
+++ src/Enum/WxEventKeys.php
@@ -5,8 +5,17 @@ namespace Ineplant\Enum;
 
 
 class WxEventKeys {
+
+    //按autoreply_messages的id查询回复
+    const MESSAGES_REPLAY = 'msg_';
+
+    //关键词回复
+    const KEYWORDS_REPLAY = 'kw_';
+
     const SHARE_SUBSCRIBE = '_share__';
     const COMPANY_SHARE_SUBSCRIBE = '_companyshare__';
     const LOGIN = '_log__';
+
+    //回复活动图文消息 $activityId
     const ACTIVITY = '_activity__';
 }
