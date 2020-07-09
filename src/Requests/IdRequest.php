1f73962b83f3d1ef27496e1186c40fae8395e322
thxcud <thxcud@outlook.com>
2020-07-09 15:51:11 +0800

    request

---
+++ src/Requests/IdRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Ineplant\Requests;
+
+use Ineplant\BaseRequest;
+
+class IdRequest extends BaseRequest {
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize() {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules() {
+        return [
+            'id' => 'required|integer|min:1'
+        ];
+    }
+}
